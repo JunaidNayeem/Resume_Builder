f8b2127f051d1b0e137293e866e55048c403c337
JunaidNayeem <junaid85210@gmail.com>
2023-04-06 01:53:48 +0530

    First commit

---
+++ src/components/layout/header.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./styles/header.less";
+
+const header = () => {
+  return <div class="header-conatiner">Resume Builder</div>;
+};
+
+export default header;
---
+++ src/components/layout/layout.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./styles/layout.less";
+
+const layout = () => {
+  return (
+    <div className="main-conatiner">
+      <div className="Form-conatiner">
+        <form action="post"></form>
+      </div>
+      <div className="demo-conatiner">
+        <div className="resume-page"></div>
+      </div>
+    </div>
+  );
+};
+
+export default layout;
